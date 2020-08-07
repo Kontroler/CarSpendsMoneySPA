4c0aa4340fdd89f792bf3fb4d6eaf61da65dab19
r.nowowieski <r.nowowieski@siden.pl>
2020-08-07 11:05:41 +0200

    added login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertifyService } from './_services/alertify.service';
+import { ErrorInterceptorProvider } from './_interceptors/ErrorInterceptor';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthService } from './_services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -6,11 +12,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, MenuComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,3 +1,5 @@
+import { AlertifyService } from './../../_services/alertify.service';
+import { AuthService } from './../../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
   model: any = {};
 
-  constructor() {}
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in successfully');
+      },
+      (error) => {
+        this.alertify.error('Failed to login');
+      }
+    );
+  }
+
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('Logged out');
+  }
 }
